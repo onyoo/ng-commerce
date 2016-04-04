1aa8250fee49779dc911a2eebae484deb80a66bd
onyoo <roberto9m9@gmail.com>
2016-04-04 17:41:30 +0200

    adds product quantity and (verbosely) prevents duplicates serverside

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -10,12 +10,18 @@ class CartsController < ApplicationController
   end
 
   def update
-    user = User.find_by(name: params[:id])
+    cart_products = User.find_by(name: params[:id]).carts.last.products
     product = Product.find(params[:product_id])
-    if user.carts.last.products << product
+    if !cart_products.include? product
+      cart_products << product
       product.inventory -= 1
-      product.save
+    else
+      cart_product = cart_products.find(product.id)
+      cart_product.quantity += 1
+      product.inventory -= 1
+      cart_product.save
     end
+    product.save
     render nothing: true, status: 202
   end
 
---
+++ db/migrate/20160404152521_add_quanity_to_product.rb
@@ -0,0 +1,5 @@
+class AddQuanityToProduct < ActiveRecord::Migration
+  def change
+    add_column :products, :quantity, :integer, default: 0
+  end
+end
