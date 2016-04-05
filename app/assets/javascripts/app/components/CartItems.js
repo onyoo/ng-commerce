7fa6540335ff30e15bdf52f6a5e776a73097766c
onyoo <roberto9m9@gmail.com>
2016-04-05 22:51:53 +0200

    cart_item refactor -clearer naming

--- app/assets/javascripts/app/components/CartItems.js
+++ app/assets/javascripts/app/components/CartItems.js
@@ -7,11 +7,12 @@ var CartItem = {
     var ctrl = this;
 
     ctrl.cartTotal = [].map(function(a) {return a.quantity});
+
     ctrl.getTotal = function(){
         var total = 0;
 
-        for(var i = 0; i < ctrl.data.length; i++){
-            var product = ctrl.data[i];
+        for(var i = 0; i < ctrl.cartItems.length; i++){
+            var product = ctrl.cartItems[i];
             total += product.price;
         };
         return total;
@@ -20,17 +21,18 @@ var CartItem = {
     ctrl.changeQuant = function(cartId, productId) {
       showQuantForm = false;
       CartService.changeQuant(cartId, productId, this.quantity).success(function(cartItems) {
-        ctrl.data = cartItems;
+        ctrl.cartItems = cartItems;
       });
+      ctrl.quantity = undefined;
     };
 
 
     CartService
       .getContents(this.id)
       .then(function(resp) {
-        ctrl.data = resp.data;
-        for( var i = 0; i < ctrl.data.length; i++){
-          ctrl.cartTotal.push(ctrl.data[i].price);
+        ctrl.cartItems = resp.data;
+        for( var i = 0; i < ctrl.cartItems.length; i++){
+          ctrl.cartTotal.push(ctrl.cartItems[i].price);
         };
       });
 
