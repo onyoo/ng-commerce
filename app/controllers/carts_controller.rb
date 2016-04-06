703d0d53a355cb95bf8641ac4f63ecf1bc85283d
onyoo <roberto9m9@gmail.com>
2016-04-06 10:13:20 +0200

    refactor cart_items and product_items and ng-show work

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,12 +5,7 @@ class CartsController < ApplicationController
   end
 
   def show
-    if (Integer(params[:id]) rescue false)
-      render json: Cart.find(params[:id]).products
-    else
-      user = User.find_by(name: params[:id])
-      render json: Cart.where(user_id: user.id)
-    end
+    render json: Cart.find(params[:id]).products
   end
 
   def update
